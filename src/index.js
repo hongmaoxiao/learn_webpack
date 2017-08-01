8ab085876dbc453fbc43f6f7e36009d811a23d00
Xiaomao <fxm@autoforce.net>
2017-08-01 11:22:53 +0800

    load image success

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Rain from './rain.png';
 
 function component() {
   var element = document.createElement("div");
@@ -7,6 +8,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Rain;
+
+  element.appendChild(myIcon);
 
   return element;
 }
